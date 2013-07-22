a6c33d08a374564d03ec4d9f4bafe70a9d88d86d
gnemoug <gnemoug@gmail.com>
2013-07-22 10:43:33 +0800

    init project

---
+++ douban-album/gen/com/gnemoug/douban_album/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.gnemoug.douban_album;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
